cfe48674c3c9fbc220bb50877668ba689f3795bc
ZENILTON <zeniltonccardoso@gmail.com>
2020-09-01 22:59:52 -0300

    Add marcadores e localização

--- app/src/main/java/com/example/maps/MapsFragment.kt
+++ app/src/main/java/com/example/maps/MapsFragment.kt
@@ -1,22 +1,39 @@
 package com.example.maps
 
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.location.Location
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.maps.CameraUpdateFactory
 
 
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var lastLocation: Location
+
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -36,5 +53,56 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         mMap = googleMap
 
         mMap.uiSettings.isZoomControlsEnabled = true
+    }
+
+    private fun placeMarker(location: LatLng) {
+        val Havan = MarkerOptions().position(LatLng(-27.099601, -48.906891))
+        val Havan_Porto_Belo = MarkerOptions().position(LatLng(-27.1597896, -48.5758767))
+
+        Havan_Porto_Belo.icon(
+            BitmapDescriptorFactory.fromBitmap(
+                BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
+            )
+        )
+            .title("Havan Porto Belo")
+            .snippet("Rod. Antônio Heil, 250 - Centro 1,")
+            .snippet("Brusque - SC - 88353-100")
+        mMap.addMarker(Havan_Porto_Belo)
+
+        Havan.icon(
+            BitmapDescriptorFactory.fromBitmap(
+                BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher)
+            )
+        )
+            .title("Havan")
+            .snippet("Rod. Antônio Heil, 250 - Centro 1,")
+            .snippet("Brusque - SC - 88353-100")
+        mMap.addMarker(Havan)
+    }
+
+    private fun setUpMap() {
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                requireActivity(),
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        }
+
+        mMap.isMyLocationEnabled = true
+        fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+
+            if (location != null) {
+                lastLocation = location
+                val currentLatLong = LatLng(location.latitude, location.longitude)
+                placeMarker(currentLatLong)
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLong, 12f))
+            }
+        }
     }
 }
